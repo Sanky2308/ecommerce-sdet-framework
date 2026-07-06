48c03b09adbac91004cc7257844ff1e1bcb3db0b
Sanky2308 <106574800+Sanky2308@users.noreply.github.com>
2026-07-05 22:10:36 -0400

    Add TestNG smoke test class

---
+++ src/test/java/sample/SmokeNg.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SmokeNg {
+
+    @Test
+    public void shouldRun() {
+        System.out.println("TestNG smoke test executed");
+    }
+}
