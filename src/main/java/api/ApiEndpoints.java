381ce251ec16079321456b24cf589fa0cfd17dfc
Sanky2308 <106574800+Sanky2308@users.noreply.github.com>
2026-07-05 22:11:04 -0400

    Add API endpoint constants

---
+++ src/main/java/api/ApiEndpoints.java
@@ -0,0 +1,10 @@
+package api;
+
+public final class ApiEndpoints {
+    public static final String PRODUCTS_LIST = "/api/productsList";
+    public static final String BRANDS_LIST = "/api/brandsList";
+    public static final String VERIFY_LOGIN = "/api/verifyLogin";
+
+    private ApiEndpoints() {
+    }
+}
